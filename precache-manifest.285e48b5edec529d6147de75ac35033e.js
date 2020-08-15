789dd76ea7b998caf43ea41acb1b27b50ab9fd88
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-08-15 16:08:25 +0530

    Updates

---
+++ precache-manifest.285e48b5edec529d6147de75ac35033e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a554bbcc4a2400980f2a852cb42b3754",
+    "url": "/exchange_rate-V2.0/index.html"
+  },
+  {
+    "revision": "935708c541956d9cc2ce",
+    "url": "/exchange_rate-V2.0/static/css/main.3a037d9c.chunk.css"
+  },
+  {
+    "revision": "f727e75c0e9b1d434081",
+    "url": "/exchange_rate-V2.0/static/js/2.fef2bf65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exchange_rate-V2.0/static/js/2.fef2bf65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935708c541956d9cc2ce",
+    "url": "/exchange_rate-V2.0/static/js/main.e3dfedfe.chunk.js"
+  },
+  {
+    "revision": "25bb44fbe758e119b178",
+    "url": "/exchange_rate-V2.0/static/js/runtime-main.cc978e37.js"
+  }
+]);
